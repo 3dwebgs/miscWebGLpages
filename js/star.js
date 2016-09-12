4c2cea6484018d06a689000dabe26a33b3eb4d40
3dwebgs <3dwebgs@users.noreply.github.com>
2016-09-12 08:34:36 -0700

    Update star.js

--- js/star.js
+++ js/star.js
@@ -1,36 +1,36 @@
 
 
-	var Star = function (position, size, scene) {
-		this.sphere = BABYLON.Mesh.CreateSphere("sphere1", 50, 50*size, scene); 
+	var YellowStar = function (position, size, scene) {
+		this.sphere = BABYLON.Mesh.CreateSphere("sphere1", 30, 30*size, scene); 
 		this.mat = new BABYLON.StandardMaterial("white", scene);
 		this.mat.diffuseTexture = new BABYLON.Texture("textures/suntexture.jpg", scene);
 		this.mat.specularColor = new BABYLON.Color3(0, 0, 0);
 		this.sphere.material = this.mat;
 		this.sphere.position = position;
 		
-
-		this.particleSystem = new BABYLON.ParticleSystem("particles", 1500, scene);
+		this.particleSystem = new BABYLON.ParticleSystem("particles", 400, scene);
 		this.particleSystem.particleTexture = new BABYLON.Texture("textures/fireflare.jpg", scene);
 		this.particleSystem.emitter = this.sphere;
 		this.particleSystem.color1 = new BABYLON.Color4(0.984, 0.337, 0.047, 1);
 		this.particleSystem.color2 = new BABYLON.Color4(0.984, 0.757, 0.047, 1);
 		this.particleSystem.minSize = 16*size;
-		this.particleSystem.maxSize = 60*size;
-		this.particleSystem.minLifeTime = 0.25*size;
-		this.particleSystem.maxLifeTime = 0.25*size;
-		this.particleSystem.emitRate = 1500;
+		this.particleSystem.maxSize = 40*size;
+		this.particleSystem.minLifeTime = 0.20*size;
+		this.particleSystem.maxLifeTime = 0.20*size;
+		this.particleSystem.emitRate = 300;
 		this.particleSystem.direction1 = new BABYLON.Vector3(-120, -120, -120);
 		this.particleSystem.direction2 = new BABYLON.Vector3(120, 120, 120);
 		this.particleSystem.minAngularSpeed = 0;
 		this.particleSystem.maxAngularSpeed = Math.PI;
-		this.particleSystem.minEmitPower = 1;
-		this.particleSystem.maxEmitPower = 3;
+		this.particleSystem.minEmitPower = 0.5;
+		this.particleSystem.maxEmitPower = 2;
 		this.particleSystem.updateSpeed = 0.01;
 		this.particleSystem.start();
 	};
 
+
 	var WhiteStar = function(position, size, scene){
-		this.sphere = BABYLON.Mesh.CreateSphere("sphere1", 25, 25*size, scene); 
+		this.sphere = BABYLON.Mesh.CreateSphere("whiteStar", 20, 15*size, scene); 
 		this.mat = new BABYLON.StandardMaterial("white", scene);
 		this.mat.emissiveColor = new BABYLON.Color3(1, 1, 1);
 		this.sphere.material = this.mat;
@@ -38,3 +38,12 @@
 		
 
 	}
+
+	var RedStar = function (position, size, scene) {
+		this.sphere = BABYLON.Mesh.CreateSphere("redStar", 20, 30*size, scene); 
+		this.mat = new BABYLON.StandardMaterial("red", scene);
+		this.mat.emissiveColor = new BABYLON.Color3(0.714, 0.239, 0.169);
+		this.mat.specularColor = new BABYLON.Color3(0, 0, 0)
+		this.sphere.material = this.mat;
+		this.sphere.position = position;
+	};
